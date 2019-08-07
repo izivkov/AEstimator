66e9e71a9f2db1cfb29099c38f7fa7ccdc726c78
Ivo Zivkov <izivkov@gmail.com>
2019-08-07 18:24:34 -0400

    WIP

--- app/src/main/java/org/avmedia/mirrormirror/utils/FileUploader.kt
+++ app/src/main/java/org/avmedia/mirrormirror/utils/FileUploader.kt
@@ -7,6 +7,8 @@ import com.github.kittinunf.fuel.core.FileDataPart
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.coroutines.awaitObjectResult
+import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
 import kotlinx.coroutines.runBlocking
 import org.json.JSONObject
 import java.io.File
@@ -34,25 +36,55 @@ open class FileUploader(baseUrl: URL, val successCallback: (msg: JSONObject) -> 
                     .progress { writtenBytes, totalBytes ->
                         val progress = writtenBytes.toFloat() / totalBytes.toFloat()
                         Log.v(TAG, "Upload: ${progress}")
-                        progressCallback.invoke((100*progress).toInt())
+                        progressCallback.invoke((100 * progress).toInt())
                     }
                     .also { Log.d(TAG, it.toString()) }
 
                     .awaitObjectResult(AgeDeserializer).fold(
-                            { data -> successCallback.invoke(JSONObject(data))},
-                            { error -> failureCallback.invoke(JSONObject("""{"msg": "Something went wrong!"}""")) }
+                            { data ->
+                                successCallback.invoke(JSONObject(data))
+                            },
+                            { error ->
+                                failureCallback.invoke(JSONObject("""{"msg": "Something went wrong!"}"""))
+                            }
                     )
         }
-
     }
+
+    open fun upload_XXX(file: File) {
+        val fileSize = file.length()
+        Fuel.upload("/model/predict")
+                .add {
+                    FileDataPart(file, "image", "testimage.jpg")
+                }
+
+                .progress { writtenBytes, totalBytes ->
+                    val progress = writtenBytes.toFloat() / totalBytes.toFloat()
+                    Log.v(TAG, "Upload: ${progress}")
+                    progressCallback.invoke((100 * progress).toInt())
+                }
+                .also { Log.d(TAG, it.toString()) }
+
+                .responseJson { _, _, result ->
+
+                    when (result) {
+                        is Result.Success -> {
+                            successCallback.invoke(JSONObject(result.get().content))
+                        }
+                        is Result.Failure -> {
+                            failureCallback.invoke(JSONObject("""{"msg": "Something went wrong!"}"""))
+                        }
+                    }
+                }
+    }
+
 
     object AgeDeserializer : ResponseDeserializable<String> {
         override fun deserialize(content: String) =
                 content
     }
 
-    open fun setProgressListener (_progressCallback: (progress: Int) -> Unit) {
+    open fun setProgressListener(_progressCallback: (progress: Int) -> Unit) {
         this.progressCallback = _progressCallback
-
     }
 }
